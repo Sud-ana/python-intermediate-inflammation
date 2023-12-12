13ca8565175ee70fd0392c148806486d4d4a0b1e
Sudhansu Mishra <engr.sudhansu.mishra@gmail.com>
2023-12-12 15:42:48 +0000

    Added a Patient and person class. Then added unit tests for this class.

--- inflammation/models.py
+++ inflammation/models.py
@@ -8,7 +8,7 @@ and each column represents a single day across all patients.
 """
 
 import numpy as np
-
+from functools import reduce
 
 def load_csv(filename):
     """Load a Numpy array from a CSV
@@ -66,3 +66,78 @@ def patient_normalise(data):
         normalised = data / max_data[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
     return normalised
+
+
+# def daily_above_threshold(patient_num, data, threshold):
+#   """Determine whether or not each daily inflammation value exceeds a given threshold for a given patient.
+
+#   :param patient_num: The patient row number
+#   :param data: A 2D data array with inflammation data
+#   :param threshold: An inflammation threshold to check each daily value against
+#   :returns: A boolean list representing whether or not each patient's daily inflammation exceeded the threshold
+#   """
+
+#   return list(map(lambda x: x > threshold, data[patient_num]))
+
+
+
+def daily_above_threshold(patient_num, data, threshold):
+  """Count how many days a given patient's inflammation exceeds a given threshold.
+
+  :param patient_num: The patient row number
+  :param data: A 2D data array with inflammation data
+  :param threshold: An inflammation threshold to check each daily value against
+  :returns: An integer representing the number of days a patient's inflammation is over a given threshold
+  """
+  def count_above_threshold(a, b):
+      if b:
+          return a + 1
+      else:
+          return a
+
+  # Use map to determine if each daily inflammation value exceeds a given threshold for a patient
+  above_threshold = map(lambda x: x > threshold, data[patient_num])
+  # Use reduce to count on how many days inflammation was above the threshold for a patient
+  return reduce(count_above_threshold, above_threshold, 0)
+  # above_threshold = map(lambda x: x > threshold, data[patient_num])
+  # return reduce(lambda a, b: a + 1 if b else a, above_threshold, 0)
+  # return list(map(lambda x: x > threshold, data[patient_num]))
+
+
+class Person:
+    """A person."""
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.observations = []
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+            except IndexError:
+                day = 0
+        new_observation = Observation(day, value)
+        self.observations.append(new_observation)
+        return new_observation
+
+class Doctor(Person):
+    """A doctor in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, new_patient):
+        # A crude check by name if this patient is already looked after
+        # by this doctor before adding them
+        for patient in self.patients:
+            if patient.name == new_patient.name:
+                return
+        self.patients.append(new_patient)
